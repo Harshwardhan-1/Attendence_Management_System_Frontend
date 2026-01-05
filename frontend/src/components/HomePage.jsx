d4c305517b2abac586b9ab418dddea53c45e53f7
harsh <harshwardhany87@gmail.com>
2026-01-05 10:14:40 +0530

    added

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -2,10 +2,9 @@ import './HomePage.css';
 export default function HomePage({userData}){
     return(
         <>
-        <div className="homepage-container">
         <h1>Welcome to HomePage</h1>
         <p>{userData?.name}</p>
-        </div>
+
         </>
     );
 }     
