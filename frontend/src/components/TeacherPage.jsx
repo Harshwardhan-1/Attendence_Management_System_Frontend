1b3aea64a645986b81648d1cd479b78458eae03c
harsh <harshwardhany87@gmail.com>
2026-01-08 15:21:33 +0530

    teacher can make present

--- frontend/src/components/TeacherPage.jsx
+++ frontend/src/components/TeacherPage.jsx
@@ -37,6 +37,21 @@ export default function TeacherPage(){
             }
         }
     }
+    const handlePresent=async(name,gmail,section)=>{
+        const send={name,gmail,section};
+        try{
+    const response=await axios.post('https://event-managaement-system-backend.onrender.com/api/TeacherAttendence/markAttendence',send,{withCredentials:true});
+    if(response.data.message=== 'student attendence successfully marked present'){
+        alert('attendence marked successfully');
+    }
+}catch(err){
+    if(err.response?.data?.message=== 'provide detail properly'){
+        alert('fill detail properly');
+    }else if(err.response?.data?.message=== 'student attendence mark already for today subject'){
+        alert('student attendence for this subject and for this date already marked previously');
+    }
+}
+    }
     return(
         <>
           <div className="teacher-page">
@@ -50,18 +65,23 @@ export default function TeacherPage(){
                 <p><span>Section:</span>{data?.section}</p>
     <button onClick={()=>handleStudents(data?.department,data?.section)}>Show All Students of this class</button>
                 </div>   
-                </div>
-
+    
+ <div className="students-container">
                 {student.map((all,index)=>(
-                    <div key={index}>
+                    <div className="student-card" key={index}>
                         <p>{all?.userId?.name}</p>
                         <p>{all?.userId?.gmail}</p>
                         <p>{all?.rollNo}</p>
                         <p>{all?.branch}</p>
-                        <p>{all.section}</p>
-                        <p>{all.semester}</p>
+                        <p>{all?.section}</p>
+                        <p>{all?.semester}</p>
+<button onClick={()=>handlePresent(all?.userId?.name,all?.userId?.gmail,all?.section)}>Present</button>
+                        <button>Absent</button>
+                        <button>Show All Attendence</button>
                     </div>
                 ))}
+                </div>
+                </div>
         </>
     );
 }
